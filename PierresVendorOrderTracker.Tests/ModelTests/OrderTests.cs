6b3d3866f7ef675766cc56b45abe4ae525ed364f
object-ions <moshikoatia@gmail.com>
2023-09-29 17:24:11 -0400

    run and pass first test- create instance of Order

---
+++ PierresVendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierresVendorOrderTracker.Models;
+using System;
+
+namespace PierresVendorOrderTracker.Tests
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        public void OrderConstructor_CreatesInstanceOfOrder_Order()
+        {
+            // Arrange
+            Order newOrder = new Order("test");
+            // Act
+
+            // Assert
+            Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+    }
+}
