96de1a3bf89de6b8b5c00eecf134d96ff25ed041
object-ions <moshikoatia@gmail.com>
2023-09-30 15:56:31 -0400

    pass 5th test - returns correct vendor from Find()

--- PierresVendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ PierresVendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -45,8 +45,8 @@ namespace PierresVendorOrderTracker.Tests
     [TestMethod]
     public void GetAll_ReturnsAllVendorObjects_VendorList()
     {
-      string name01 = "Work";
-      string name02 = "School";
+      string name01 = "Suzi Cafe";
+      string name02 = "Susu 'n' Sons";
       Vendor newVendor1 = new Vendor(name01);
       Vendor newVendor2 = new Vendor(name02);
       List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
@@ -54,6 +54,19 @@ namespace PierresVendorOrderTracker.Tests
       List<Vendor> result = Vendor.GetAll();
 
       CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string name01 = "Suzi Cafe";
+      string name02 = "Susu 'n' Sons";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+
+      Vendor result = Vendor.Find(2);
+
+      Assert.AreEqual(newVendor2, result);
     }
   }
 }
