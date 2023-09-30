f3233def0b835fb2c650bd51c6ff2e3c295db516
object-ions <moshikoatia@gmail.com>
2023-09-30 18:24:20 -0400

    adjust files and directories to follow RESTful conventions

--- PierresVendorOrderTracker/Controllers/OrdersController.cs
+++ PierresVendorOrderTracker/Controllers/OrdersController.cs
@@ -25,5 +25,22 @@ namespace PierresVendorOrderTracker.Controllers
       Order foundOrder = Order.Find(id);
       return View(foundOrder);
     }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
   }
 }
